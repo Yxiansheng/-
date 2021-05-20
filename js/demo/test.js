419792ca24023936d4eb996ac4b58f2b2e514d5d
姚浩楠 <yaohaonan@xiao100.com>
2021-05-20 12:16:37 +0800

    feat: 增加 js、css 部分常见问题

---
+++ js/demo/test.js
@@ -0,0 +1,30 @@
+Promise.all = (quque) => new Promise((resolve, reject) => {
+    const n = quque.length
+    const result = new Array(n)    
+    let overCount = 0
+
+    quque.forEach((task, index) => {
+        Promise.resolve(task).then((res) => {
+            result[index] = res
+            overCount += 1
+            if (overCount === n) {
+                resolve(result)
+            }
+        }).catch(err => {
+            reject(err)
+        })
+    })
+})
+
+const task1 = new Promise((resolve, reject) => setTimeout(() => resolve(1), 100))
+const task2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 200))
+const task3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 300))
+const task4 = 3
+
+Promise.all([task1, task2, task3, task4]).then(res => {
+    console.log('res: ', res);
+}).catch(err => {
+    console.log('err', err)
+})
+
+Promise.resolve(() => 1).then(val => console.log(val));
