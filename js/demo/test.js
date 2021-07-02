8691f1560e347977bb6d8fecbb56a53efb79e5ec
姚浩楠 <yaohaonan@xiao100.com>
2021-07-02 17:03:43 +0800

    feat: 遍历文档

--- js/demo/test.js
+++ js/demo/test.js
@@ -1,35 +1,34 @@
-Promise.all = (quque) => new Promise((resolve, reject) => {
-    const n = quque.length
-    const result = new Array(n)    
-    let overCount = 0
+const map = new Map()
+map.set('Array', [1, 2, 3, 4])
+map.set('Set', new Set([1, 2, 3, 4]))
+map.set('String', 'Hello!')
+map.set('Map', new Map([[0, 1], [1, 2], [2, 3], [3, 4]]))
 
-    quque.forEach((task, index) => {
-        Promise.resolve(task).then((res) => {
-            result[index] = res
-            overCount += 1
-            if (overCount === n) {
-                resolve(result)
-            }
-        }).catch(err => {
-            reject(err)
-        })
-    })
-})
-
-const task1 = new Promise((resolve, reject) => setTimeout(() => resolve(1), 100))
-const task2 = new Promise((resolve, reject) => setTimeout(() => resolve(2), 200))
-const task3 = new Promise((resolve, reject) => setTimeout(() => resolve(3), 300))
-const task4 = 3
-
-Promise.all([task1, task2, task3, task4]).then(res => {
-    console.log('res: ', res);
-}).catch(err => {
-    console.log('err', err)
-})
-
-Promise.resolve(() => 1).then(val => console.log(val));
-
-function test(...args) {
-    console.log(args);
+for (let [type, iterator] of map) {
+    let str = ''
+    for (let value of iterator) {
+        str += `${value}, `;
+    }
+    console.log(`${type}：${str.slice(0, -2)}`);
 }
-test(1, 22)
+
+function func () {
+    let str = ''
+    for (let arg of arguments) {
+        str += `${arg}, `;
+    }
+    console.log(`arguments：${str.slice(0, -2)}`)
+}
+func('arg1', 'arg2', 'arg3', 'arg4')
+
+const interator = 'ssdsd'[Symbol.iterator]()
+
+console.log('interator: ', interator);
+let cur = interator.next()
+while (!cur.done) {
+    console.log(cur.value);
+    cur = interator.next()
+}
+for (let char in 'hello world!') {
+    console.log('char: ', char);
+}
